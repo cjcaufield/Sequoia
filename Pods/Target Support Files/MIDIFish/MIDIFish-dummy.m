c78cb61fdf3fd02e236a1de76c8e6fe28eac8199
Colin Caufield <cjcaufield@gmail.com>
2017-08-12 23:15:39 -0700

    Removing Pods and making schemes shared.

--- Pods/Target Support Files/MIDIFish/MIDIFish-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MIDIFish : NSObject
-@end
-@implementation PodsDummy_MIDIFish
-@end
--- Pods/Target Support Files/Pods-Sequoia/Pods-Sequoia-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sequoia : NSObject
-@end
-@implementation PodsDummy_Pods_Sequoia
-@end
